977a529ce12a435adbf711a8da8247a8f2713abf
oM <omtirodkar03@gmail.com>
2026-01-17 18:47:44 +0800

    Add Greet & Exit functionality

--- src/main/java/Snoopy.java
+++ src/main/java/Snoopy.java
@@ -1,11 +1,20 @@
 public class Snoopy {
+
+    private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____                                      \n"
+        String logo = "\n ____                                      \n"
                 + "/ ___| _ __   ___   ___  _ __  _   _   \n"
                 + "\\___ \\| '_ \\ / _ \\ / _ \\| '_ \\| | | |  \n"
                 + " ___) | | | | (_) | (_) | |_) | |_| |  \n"
                 + "|____/|_| |_|\\___/ \\___/| .__/ \\__, |  \n"
                 + "                        |_|    |___/   \n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from" + logo);
+
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE);
     }
 }
