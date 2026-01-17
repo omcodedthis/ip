046f6cd6261ecfb6e9fe1998fc78c4c44b3bc12f
oM <omtirodkar03@gmail.com>
2026-01-17 18:37:49 +0800

    Rename Duke to Snoopy

---
+++ src/main/java/Snoopy.java
@@ -0,0 +1,11 @@
+public class Snoopy {
+    public static void main(String[] args) {
+        String logo = " ____                                      \n"
+                + "/ ___| _ __   ___   ___  _ __  _   _   \n"
+                + "\\___ \\| '_ \\ / _ \\ / _ \\| '_ \\| | | |  \n"
+                + " ___) | | | | (_) | (_) | |_) | |_| |  \n"
+                + "|____/|_| |_|\\___/ \\___/| .__/ \\__, |  \n"
+                + "                        |_|    |___/   \n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
