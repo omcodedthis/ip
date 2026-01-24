c77df119f7332fa2eb4b43ca0b33c1041f17d433
oM <omtirodkar03@gmail.com>
2026-01-24 19:35:01 +0800

    Add Echo functionality

--- src/main/java/Snoopy.java
+++ src/main/java/Snoopy.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Snoopy {
 
     private static final String HORIZONTAL_LINE = "____________________________________________________________";
+
+    public static void echoInput(String input) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Snoopy says:");
+        System.out.println(input);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void getCommandAndEcho() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Your input: ");
+            String input = scanner.nextLine().trim().toLowerCase();
+
+            if (input.equalsIgnoreCase("bye")) {
+                input = "Bye. Hope to see you again soon!";
+                echoInput(input);
+                break;
+            }
+            echoInput(input);
+        }
+        scanner.close();
+    }
 
     public static void main(String[] args) {
         String logo = "\n ____                                      \n"
@@ -14,7 +39,6 @@ public class Snoopy {
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE);
+        getCommandAndEcho();
     }
 }
