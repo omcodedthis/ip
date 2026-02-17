ca09813b9f1c0dfbf29e293fef3ab869313ebb51
oM <omtirodkar03@gmail.com>
2026-02-17 23:32:55 +0800

    feat: Add javadocs for Exception classes

--- src/main/java/snoopy/exception/InvalidCommandException.java
+++ src/main/java/snoopy/exception/InvalidCommandException.java
@@ -1,5 +1,8 @@
 package snoopy.exception;
 
+/**
+ * Represents an Exception for when an invalid command error occurs.
+ */
 public class InvalidCommandException extends SnoopyException {
     public InvalidCommandException(String description) {
         super(description);
--- src/main/java/snoopy/exception/SnoopyException.java
+++ src/main/java/snoopy/exception/SnoopyException.java
@@ -1,5 +1,8 @@
 package snoopy.exception;
 
+/**
+ * Represents an Exception for when incorrect input is passed to Snoopy.
+ */
 public class SnoopyException extends Exception {
     public SnoopyException(String description) {
         super(description);
